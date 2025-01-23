a895fd45ed16033f8c23477a208cc3f780c7567a
october-03 <season_autumn@kakao.com>
2025-01-23 14:05:52 +0900

    feat: implement nonce issuance in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+import { randomUUID } from 'node:crypto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly redis: Redis | null;
+
+  constructor(private readonly redisService: RedisService) {
+    this.redis = this.redisService.getOrThrow();
+  }
+
+  async issueNonce(ip: string, walletAddr: string) {
+    const nonce = randomUUID();
+    await this.redis.set(`nonce:${ip}:${walletAddr}`, nonce, 'EX', 60 * 3);
+    return nonce;
+  }
+}
