9c7ebb82e6d30ea55a85068afd001492f7275df2
october-03 <season_autumn@kakao.com>
2025-01-23 13:58:05 +0900

    chore: configure Auth service test module

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -7,6 +9,20 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        RedisModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            config: {
+              host: configService.get('REDIS_HOST'),
+              port: configService.get('REDIS_PORT'),
+              password: configService.get('REDIS_PASSWORD'),
+            },
+          }),
+          inject: [ConfigService],
+        }),
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
